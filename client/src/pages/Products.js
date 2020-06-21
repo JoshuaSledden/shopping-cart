fd9db11e028c71f304fcd684d0ac21e6ae9a02a8
Joshua Sledden <joshuasledden@gmail.com>
2020-06-21 05:31:16 +0100

    Products page
    Simple products page creation displaying all products by fetching API data from our Express server.

--- client/src/pages/Products.js
+++ client/src/pages/Products.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import '../styles/App.css';
+import { Link } from 'react-router-dom';
 
 function Products() {
+
+  useEffect(() => {
+    fetchProducts();
+  }, [])
+
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const data = await fetch('products');
+
+    const productData = await data.json();
+    setProducts(productData);
+  }
+
   return (
     <div>
       <h1>Products Page</h1>
+      {products.map(product => (
+        <p key={product._id}>
+          <Link to={`/products/${product._id}`}>
+          {product.title}        
+          </Link>
+          - {product.price}
+        </p>
+      ))}
     </div>
   );
 }
